0e3fc228656736d1cce4b470cff1f63cb6d8d6e7
franklinAnozie <cfanozie@gmail.com>
2023-02-05 18:07:51 +0100

    refactored code into smaller functions

--- src/Pages/Employees.js
+++ src/Pages/Employees.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../context";
+import { Team, EmployeesList } from "../Teams/EmployeesList";
 import maleProfile from "../images/maleProfile.jpg";
 import femaleProfile from "../images/femaleProfile.jpg";
 
@@ -11,29 +12,13 @@ const Employees = () => {
         <main className="container">
             <div className="row justify-content-center mt-3 mb-3">
                 <div className="col-6" style={{cursor: 'pointer'}}>
-                    <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
-                        <option value='Team A'>Team A</option>
-                        <option value='Team B'>Team B</option>
-                        <option value='Team C'>Team C</option>
-                        <option value='Team D'>Team D</option>
-                    </select>
+                    {Team({selectedTeam, handleTeamSelectionChange})}
                 </div>
             </div>
             <div className="row justify-content-center mt-3 mb-3">
                 <div className="col-8">
                     <div className="card-collection">
-                        {employees.map((employee)=> {
-                            return (
-                                <div key={employee.id} id={employee.id} className={employee.teamName === selectedTeam ? 'card m-2 cursor standout' : 'card m-2 cursor'} onClick={handleEmployeeCardClick}>
-                                    <img src={employee.gender === 'male' ? maleProfile : femaleProfile} alt='Profile' className="card-img-top"/>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Full Name: {employee.fullName}</h5>
-                                        <p className="card-text"><b>Designation: </b>{employee.designation}</p>
-                                        <p>Team: {employee.teamName}</p>
-                                    </div>
-                                </div>
-                            )
-                        })}
+                        {EmployeesList({employees, selectedTeam, handleEmployeeCardClick, maleProfile, femaleProfile})}
                     </div>
                 </div>
             </div>
--- src/Teams/EmployeesList.js
+++ src/Teams/EmployeesList.js
@@ -16,3 +16,14 @@ export const EmployeesList = ({employees, selectedTeam, handleEmployeeCardClick,
     })
   )
 }
+
+export const Team = ({selectedTeam, handleTeamSelectionChange}) => {
+    return (
+            <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+                <option value='Team A'>Team A</option>
+                <option value='Team B'>Team B</option>
+                <option value='Team C'>Team C</option>
+                <option value='Team D'>Team D</option>
+            </select>
+        )
+}
